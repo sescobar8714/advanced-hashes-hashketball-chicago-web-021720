045c886eb823fe3f90e3d812d1964dd628b67909
Samuel Escobar <sescobar8714@gmail.com>
2020-02-18 16:45:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -104,13 +104,13 @@ def game_hash
           blocks: 1,
           slam_dunks: 0
         }, {
-          player_name: "Brendan Haywood",
+          player_name: "Kemba Walker",
           number: 33,
           shoe: 15,
           points: 6,
           rebounds: 12,
           assists: 12,
-          steals: 22,
+          steals: 7,
           blocks: 5,
           slam_dunks: 12
         }
